a6e2ebf04487ae0b7f8462eff41c1b2dc2713eed
Jeff Dickens <dreamgear@gmail.com>
2026-01-17 21:11:33 -0500

    Fix: use 'docker compose' instead of 'docker-compose'

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+if ! docker compose version > /dev/null 2>&1; then
+  echo 'Error: docker compose is not available.' >&2
   exit 1
 fi
 
@@ -28,17 +28,17 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:4096 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d web
+docker compose up --force-recreate -d web
 
 echo "### Deleting dummy certificate ..."
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -58,7 +58,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -68,4 +68,4 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
 
 echo "### Reloading nginx ..."
-docker-compose exec web nginx -s reload
+docker compose exec web nginx -s reload
